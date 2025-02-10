25ab160837a6f5d62eb5967dd6408193f39bfa4d
gagewilson814 <gagewilson814@u.boisestate.edu>
2025-02-10 15:27:05 -0700

    starter

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,23 @@
-/**Update this file with the starter code**/
+#include "lab.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void parse_args(int argc, char **argv) {}
+
+void sh_destroy(struct shell *sh) {}
+
+void sh_init(struct shell *sh) {}
+
+bool do_builtin(struct shell *sh, char **argv) {}
+
+char *trim_white(char *line) {}
+
+void cmd_free(char **line) {}
+
+char **cmd_parse(char const *line) {}
+
+int change_dir(char **dir) {}
+
+char *get_prompt(const char *env) {}
