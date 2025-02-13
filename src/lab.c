0c65ecce37d7b8f1454bd6906cca2f71c6c7e938
gagewilson814 <gagewilson814@u.boisestate.edu>
2025-02-12 20:39:28 -0700

    task 2 completed

--- src/lab.c
+++ src/lab.c
@@ -1,11 +1,15 @@
 #include "lab.h"
 #include <ctype.h>
+#include <errno.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
-#include <asm-generic/signal.h>
 
 void parse_args(int argc, char **argv) {
   int c;
@@ -13,7 +17,7 @@ void parse_args(int argc, char **argv) {
   while ((c = getopt(argc, argv, "v")) != -1)
     switch (c) {
     case 'v':
-      printf("lab version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+      printf("shell version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
       exit(0);
     }
 }
@@ -27,36 +31,34 @@ void sh_destroy(struct shell *sh) {
 void sh_init(struct shell *sh) {
   /* See if we are running interactively.  */
   sh->shell_terminal = STDIN_FILENO;
-  sh->shell_is_interactive = isatty (sh->shell_terminal);
+  sh->shell_is_interactive = isatty(sh->shell_terminal);
 
-  if (sh->shell_is_interactive)
-    {
-      /* Loop until we are in the foreground.  */
-      while (tcgetpgrp (sh->shell_terminal) != (sh->shell_pgid = getpgrp ()))
-        kill (- sh->shell_pgid, SIGTTIN);
+  if (sh->shell_is_interactive) {
+    /* Loop until we are in the foreground.  */
+    while (tcgetpgrp(sh->shell_terminal) != (sh->shell_pgid = getpgrp()))
+      kill(-sh->shell_pgid, SIGTTIN);
 
-      /* Ignore interactive and job-control signals.  */
-      signal (SIGINT, SIG_IGN);
-      signal (SIGQUIT, SIG_IGN);
-      signal (SIGTSTP, SIG_IGN);
-      signal (SIGTTIN, SIG_IGN);
-      signal (SIGTTOU, SIG_IGN);
-      signal (SIGCHLD, SIG_IGN);
+    /* Ignore interactive and job-control signals.  */
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGCHLD, SIG_IGN);
 
-      /* Put ourselves in our own process group.  */
-      sh->shell_pgid = getpid ();
-      if (setpgid (sh->shell_pgid, sh->shell_pgid) < 0)
-        {
-          perror ("Couldn't put the shell in its own process group");
-          exit (1);
-        }
-
-      /* Grab control of the terminal.  */
-      tcsetpgrp (sh->shell_terminal, sh->shell_pgid);
-
-      /* Save default terminal attributes for shell.  */
-      tcgetattr (sh->shell_terminal, &sh->shell_tmodes);
+    /* Put ourselves in our own process group.  */
+    sh->shell_pgid = getpid();
+    if (setpgid(sh->shell_pgid, sh->shell_pgid) < 0) {
+      perror("Couldn't put the shell in its own process group");
+      exit(1);
     }
+
+    /* Grab control of the terminal.  */
+    tcsetpgrp(sh->shell_terminal, sh->shell_pgid);
+
+    /* Save default terminal attributes for shell.  */
+    tcgetattr(sh->shell_terminal, &sh->shell_tmodes);
+  }
 }
 
 bool do_builtin(struct shell *sh, char **argv) {
